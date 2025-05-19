74fd79b9a5b4208d7fc8eb492508aecdbb1a1e4e
Joseph <46531547+Silomalo@users.noreply.github.com>
2025-05-20 00:26:00 +0300

    fix: update nuxt app port from 3005 to 3003

--- nuxt/ecosystem.config.cjs
+++ nuxt/ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'nuxt',
-        port: '3005',
+        port: '3003',
         exec_mode: 'cluster',
         instances: 'max',
         script: './.output/server/index.mjs'
